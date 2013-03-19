168576fd65b80c6dd1e973de618bdac02e5c6bc2
Lukasz Koprowski <azram19@gmail.com>
2013-03-19 00:01:47 +0000

    A quick plan

---
+++ app.py
@@ -0,0 +1,9 @@
+#connect to redis
+#check if me have the list of movies
+
+#create a dictionary of words
+
+#go through each movie
+#for each movie download subtitles
+#process subtitles
+#save to a database
